bd44891211262b67d98a16e00036aa943c59efe2
krasseman <32762094+krasseman@users.noreply.github.com>
2018-01-06 15:43:49 +0100

    Kundvagn Slideshow etc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var mainCatList;
-   
+    var shoppingCart = [];
     
     
     fetch("./huvudkategorier.json")
@@ -28,7 +28,21 @@ $(document).ready(function(){
             productList = produkter;
         });
     
-   
+        var slideIndex = 0;
+        showSlides();
+        
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none"; 
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1} 
+            slides[slideIndex-1].style.display = "block"; 
+            setTimeout(showSlides, 3000); // Change image every 2 seconds
+        }     
+    
     function printMainCat(){
         $(".mainMenuList").append("<li><a href='index.html'>START</a></li>");
         for(var i = 0; i < mainCatList.length; i++){
@@ -41,7 +55,8 @@ $(document).ready(function(){
     
         
      
-    }
+        $(".cartLink").click(function(){
+            showShoppingCart();
         });
     
         printSubCat = function(i){      
@@ -91,10 +106,81 @@ $(document).ready(function(){
                 }
             }
         }
-        
-
+        addToCart = function(i){
+            
+            var productName = productList[i-1].prodName
+            var productPrice = productList[i-1].prodPrice
+            
     
-         });
+            shoppingCart.push(productList[i-1]);
+            $("#counter").html(shoppingCart.length);
+          
+        }
+    
+        showShoppingCart = function(){
+            $(".main").html("<div class='cartTitle'><h2>Kundvagn</h2></div><hr class='productHR'>");
+    
+            var priceTotal = 55;
+            for(var i = 0; i < shoppingCart.length; i++) {
+                priceTotal += shoppingCart[i].prodPrice;
+            }
+            $(".cartTitle").append("<h3>Totalpris: " + priceTotal + " kr</h3>");
+    
+    
+    
+            var json_str = JSON.stringify(shoppingCart);
+            localStorage.shoppingCart = json_str; 
+            
+            var loopCart = JSON.parse(localStorage.shoppingCart);
+    
+           
+            var cartListProdName = "<ul class='cartListProdName'>";
+            var cartListProdPrice = "<ul>";
+            var cartListRemove = "<ul class='cartListRemove'>";
+    
+            for(var i = 0; i < loopCart.length; i++){
+        
+                cartListProdName += "<li>" + loopCart[i].prodName + "</li>";
+                cartListProdPrice += "<li>" + loopCart[i].prodPrice + " kr</li>";
+                cartListRemove += "<li><a href='#' onClick='delCartItem(" + i + ")'>Ta bort</a></li>";
+            }
+    
+            cartListProdName += "<li>Frakt</li></ul>";
+            cartListProdPrice += "<li>55 kr</li></ul>";
+            cartListRemove += "</ul>";
+           
+            
+        
+            $(".main").append("<div class='cartList'></div><div class='cartSummary'></div>");
+            $(".cartList").append(cartListProdName + cartListProdPrice + cartListRemove);
+    
+            var checkOutButton = "<button class='cartButton' onclick='checkOut()'>Gå till kassan</button>";
+            $(".cartSummary").append(checkOutButton);
+    
+        }
+    
+        delCartItem = function(i){
+            shoppingCart.splice(i, 1);
+            showShoppingCart();
+            $(".cartCounter").html(shoppingCart.length);
+            if (shoppingCart.length <= 0){
+                $(".cartLink").css("background-image", "url(img/cart.png)");
+            }
+        }
+    
+            
+    
+    
+        
+    
+    
+    }
+        
+        
+        
+        
+            
+    });
         
         
     
